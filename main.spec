80c62eb15312e721fe7758b8809974ed41daaa7f
Marcel Stimberg <marcel.stimberg@sorbonne-universite.fr>
2024-07-08 16:47:29 +0200

    various updates

--- main.spec
+++ main.spec
@@ -5,7 +5,7 @@ a = Analysis(
     ['src/main.py'],
     pathex=[],
     binaries=[],
-    datas=[('assets', 'assets')],
+    datas=[],
     hiddenimports=[],
     hookspath=[],
     hooksconfig={},
--- src/main.py
+++ src/main.py
@@ -1,11 +1,28 @@
 import os
 
-from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QHBoxLayout, QPushButton, QTextEdit, QWidget, QLabel
-import transformers
+from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QHBoxLayout, QPushButton, QTextEdit, QWidget, QLabel, QTabWidget, QScrollArea, QListWidget
+from PyQt5.QtGui import QTextCharFormat, QSyntaxHighlighter, QFont
+from PyQt5.QtCore import QRegularExpression, Qt
+
+STYLE = """QWidget {
+   font-size: 30px;
+}"""
 
 main_folder = r"/home/mstimberg/test_files"
 
 replacements = {"." : ". \\pau=500\\"}
+
+class PauseHighlighter(QSyntaxHighlighter):
+    def highlightBlock(self, text):
+        keyword_format = QTextCharFormat()
+        keyword_format.setForeground(Qt.darkBlue)
+        keyword_format.setFontWeight(QFont.Bold)
+
+        rule = QRegularExpression(r"\\pau=\d+\\")
+        rule_iterator = rule.globalMatch(text)
+        while rule_iterator.hasNext():
+            match = rule_iterator.next()
+            self.setFormat(match.capturedStart(), match.capturedLength(), keyword_format)
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -15,30 +32,76 @@ class MainWindow(QMainWindow):
         # Make window fullscreen
         self.showMaximized()
 
-        self.layout = QVBoxLayout()
+        self.central_layout = QVBoxLayout()
+        top_bar = QWidget()
+        top_bar_layout = QHBoxLayout()
+        top_bar.setLayout(top_bar_layout)
+        self.title = QLabel("Insertion de pauses")
+        top_bar_layout.addWidget(self.title)
+        self.close_button = QPushButton("X")
+        top_bar_layout.addStretch()
+        self.close_button.clicked.connect(self.close)
+        top_bar_layout.addWidget(self.close_button)
+        self.central_layout.addWidget(top_bar)
+        self.layout = QHBoxLayout()
+        top_bar.setStyleSheet("background-color: darkblue; color: white; padding: 10px;")
+
         self.central_widget = QWidget()
-        self.central_widget.setLayout(self.layout)
+        self.central_widget.setLayout(self.central_layout)
+        self.central_layout.addLayout(self.layout)
         self.setCentralWidget(self.central_widget)
 
+        # Tabbed interface for files
+        self.file_tabs = QTabWidget()
+        # Add a tab for recent files
+        self.scroll_recent = QScrollArea()
+        self.recent_file_list = QListWidget()
+        self.scroll_recent.setWidget(self.recent_file_list)
+        self.scroll_recent.setWidgetResizable(True)
+        self.scroll_recent.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOn)
+        self.scroll_recent.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        self.file_tabs.addTab(self.scroll_recent, "Fichiers récents")
+        self.file_search = QWidget()
+        self.file_tabs.addTab(self.file_search, "Recherche")
+        self.layout.addWidget(self.file_tabs, stretch=1)
+
+        self.text_layout = QVBoxLayout()
+        self.text_title_layout = QHBoxLayout()
+        filename = QLabel("Fichier: ")
         self.file_label = QLabel("")
-        self.layout.addWidget(self.file_label)
+
+        self.file_label.setStyleSheet("font-weight: bold;")
+        self.text_title_layout.addWidget(filename)
+        self.text_title_layout.addWidget(self.file_label)
+        self.text_title_layout.addStretch()
+
+        self.text_area = QHBoxLayout()
         self.textbox = QTextEdit()
-        self.layout.addWidget(self.textbox)
+        self.text_area.addWidget(self.textbox)
+        self.text_layout.addLayout(self.text_title_layout)
+        self.text_layout.addLayout(self.text_area)
 
         # Buttons
-        self.buttons = QHBoxLayout()
+        self.buttons = QVBoxLayout()
         self.pause_button = QPushButton("+ pauses")
         self.pause_button.clicked.connect(self.add_pauses)
         self.buttons.addWidget(self.pause_button)
         self.save_button = QPushButton("Enregistrer")
         self.save_button.clicked.connect(self.save)
         self.buttons.addWidget(self.save_button)
-        self.close_button = QPushButton("Fermer")
-        self.close_button.clicked.connect(self.close)
-        self.buttons.addWidget(self.close_button)
-        self.layout.addLayout(self.buttons)
+        self.buttons.addStretch()
+        self.text_area.addLayout(self.buttons)
+        self.layout.addLayout(self.text_layout, stretch=2)
         self.filename = None
+        self.highlighter = PauseHighlighter(self.textbox.document())
         self.style()
+        self.files = os.listdir(main_folder)
+        files = [(f, os.path.getmtime(os.path.join(main_folder, f))) for f in self.files]
+        files = sorted(files, key=lambda x: x[1])
+        self.recent_file_list.addItems([os.path.splitext(f)[0] for f, mtime in files])
+        self.recent_file_list.itemClicked.connect(self.change_file)
+        self.recent_file_list.setAlternatingRowColors(True)
+        self.recent_file_list.setStyleSheet("alternate-background-color: white;background-color: #eeeeee;");
 
     def add_pauses(self):
         text = self.textbox.toPlainText()
@@ -46,6 +109,12 @@ class MainWindow(QMainWindow):
             text = text.replace(*replacement)
         self.textbox.setPlainText(text)
 
+    def change_file(self, item):
+        file = os.path.join(main_folder, item.text() + ".txt")  
+        self.load_file(file)
+
+    def select_file(self):
+        pass
 
     def save(self):
         with open(self.filename, "w") as f:
@@ -69,8 +138,7 @@ def last_file_from_folder():
 
 
 def style(app):
-    with open("assets/style.css", "r") as f:
-        app.setStyleSheet(f.read())
+    app.setStyleSheet(STYLE)
 
 if __name__ == "__main__":
     app = QApplication([])
